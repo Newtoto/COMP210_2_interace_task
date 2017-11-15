e887fd1be005c83312f0ee71e46d10cef6929055
Andrew Findlay <andrewrfindlay@ntlworld.com>
2017-11-15 22:43:29 +0000

    Added unity project with steam VR integration

---
+++ RooftopSniperVR/Assets/GunController.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour {
+
+    private bool loaded = false;
+
+    // Used to only detect axis
+    private bool shootPressed = false;
+    private bool reloadPressed = false;
+
+    public GameObject bullet;
+    public Vector3 bulletSpawnPoint;
+    private Quaternion bulletRotation;
+
+    // Axis value inputs to bools
+    private void getAxisInputs()
+    {
+        if (Input.GetAxis("Shoot") > 0)
+        {
+            shootPressed = true;
+        }
+        else
+        {
+            shootPressed = false;
+        }
+
+        if (Input.GetAxis("Reload") > 0)
+        {
+            reloadPressed = true;
+        }
+        else
+        {
+            reloadPressed = false;
+        }
+    }
+
+
+    // Reload the gun
+    private void reload()
+    {
+        if (!loaded)
+        {
+            loaded = true;
+            Debug.Log("Reloading");
+        } else
+        {
+            Debug.Log("Gun is already loaded");
+        }
+    }
+
+    // Spawn bullet and require reload
+    private void shoot()
+    {
+        if (loaded)
+        {
+            loaded = false;
+            Debug.Log("Shooting");
+            Instantiate(bullet, bulletSpawnPoint, bulletRotation);
+        } else
+        {
+            Debug.Log("Gun is not loaded");
+        }
+    }
+
+
+	void Start () {
+
+        // Check if bullet is attatched to gun
+        if (bullet == null)
+        {
+            Debug.LogError("Bullet GameObject not attatched to gun");
+        }
+
+        // Set bullet rotation to match the gun
+        bulletRotation = gameObject.transform.rotation;
+
+        // Set bullet spawn to location of spawn on gun object
+        bulletSpawnPoint = GameObject.Find("BulletSpawnPoint").transform.position;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        getAxisInputs();
+
+        // Detect firing input
+        if (shootPressed)
+        {
+            shoot();
+        }
+        // Detect reload input
+        if (reloadPressed)
+        {
+            reload();
+        }
+
+	}
+}
---
+++ RooftopSniperVR/Assets/Scripts/BulletMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour {
+
+    public float BulletSpeed;
+
+    // Use this for initialization
+    void Start () {
+
+        // Set default bullet speed of 6
+        if (BulletSpeed == 0)
+        {
+            BulletSpeed = 6.0f;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Quaternion BulletRotation = gameObject.transform.rotation;
+
+        transform.position -= (transform.up) * Time.deltaTime * BulletSpeed;
+    }
+}
