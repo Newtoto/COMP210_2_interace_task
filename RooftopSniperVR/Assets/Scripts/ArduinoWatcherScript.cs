fe784bf05bdf7bf4e72046921c1e0efaba0a4f1d
Andrew Findlay <andrewrfindlay@ntlworld.com>
2017-11-16 01:40:16 +0000

    Added arduino code

---
+++ RooftopSniperVR/Assets/Scripts/ArduinoWatcherScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class ArduinoWatcherScript : MonoBehaviour {
+
+    SerialPort ArduinoPort = new SerialPort("COM3", 9600);
+    public int potentiometerValue;
+
+    // Use this for initialization
+    void Start()
+    {
+        ArduinoPort.Open();
+        ArduinoPort.ReadTimeout = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ArduinoPort.IsOpen)
+            try
+            {
+                potentiometerValue = int.Parse(ArduinoPort.ReadLine());
+            }
+            catch (System.Exception)
+            {
+
+            }
+    }
+}
---
+++ RooftopSniperVR/Assets/Scripts/Reload.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reload : MonoBehaviour {
+
+    public GameObject ArduinoWatcher;
+    public int FromArduino;
+    public Vector3 TargetPosition;
+    
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        FromArduino = ArduinoWatcher.GetComponent<ArduinoWatcherScript>().potentiometerValue;
+
+        TargetPosition = new Vector3(0.0f, 0.0f, FromArduino / 10);
+
+        gameObject.transform.position += TargetPosition - gameObject.transform.position;
+    }
+}
