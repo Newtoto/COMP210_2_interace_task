d627e7675a5e4b01439d307721ed6eb32f3bf56f
Andrew Findlay <andrewrfindlay@ntlworld.com>
2017-12-05 09:56:35 +0000

    Tweaked table calibration code to have calibration button

--- RooftopSniperVR/Assets/Scripts/GetControllerPosition.cs
+++ RooftopSniperVR/Assets/Scripts/GetControllerPosition.cs
@@ -6,10 +6,13 @@ public class GetControllerPosition : MonoBehaviour {
 
 	//private Vector3 PositionAdjustment = new Vector3 (-0.35f, -0.41f, -0.61f);
 	private Vector3 PositionAdjustment = new Vector3 (0, 0, 0);
+	private bool calibrating;
 	public GameObject RightController;
 	// Use this for initialization
 	void Start () {
-		transform.position = RightController.transform.position + PositionAdjustment;
+		calibrating = true;
+		//transform.position = RightController.transform.position + PositionAdjustment;
+		transform.position = RightController.transform.position;
 		transform.rotation = RightController.transform.rotation;
 
         var Rotation = Quaternion.Euler(0, RightController.transform.rotation.eulerAngles.y, 0); //transpose values
@@ -18,7 +21,19 @@ public class GetControllerPosition : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetKeyDown("c"))
+		{
+			Debug.Log("Table calibrated");
+			calibrating = false;
+		}
 
+		if(calibrating)
+		{
+			//transpose values
+			var Rotation = Quaternion.Euler(0, RightController.transform.rotation.eulerAngles.y, 0);
+        		transform.rotation = Rotation;
+			transform.position = RightController.transform.position;
+		}
 	}
 
 }
